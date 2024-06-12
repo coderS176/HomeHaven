973d0de530d41795fe30be7cee6c28581b358ba3
Shreya Chaudhary <116176243+SleepyheadC@users.noreply.github.com>
2024-06-12 15:50:26 +0530

    connection to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const uri = process.env.MONGO;
+
+console.log('MONGODb_URI', uri);
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Database connection successful');
+})
+.catch(err => {
+    console.error('Database connection error:', err);
+});
 
 const app = express();
 
