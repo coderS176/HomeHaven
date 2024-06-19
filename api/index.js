065cb5ee05e9e5e3bd7bbd22d8799f5bd242574f
Shreya Chaudhary <116176243+SleepyheadC@users.noreply.github.com>
2024-06-19 23:22:32 +0530

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 
 
+
 dotenv.config();
 
 const uri = process.env.MONGO;
@@ -30,33 +31,17 @@ app.listen(3000, ()=>{
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
+// this is a middleware
+// it provies a more comprehnesive error message when any error occur
+// like inserting duplicate 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode ||500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  });
+})
 
 
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-// const userRouter = require('./routes/user.route.js');
-// const authRouter = require('./routes/auth.route.js');
-
-// dotenv.config();
-
-// const uri = process.env.MONGODB_URI;
-
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log('Connected to MongoDB');
-//   })
-//   .catch(err => {
-//     console.error('Database connection error:', err);
-//   });
-
-// const app = express();
-
-// app.use(express.json());
-
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000!!!');
-// });
-
-// app.use('/api/user', userRouter);
-// app.use('/api/auth', authRouter);
