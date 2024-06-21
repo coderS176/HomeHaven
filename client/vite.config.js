611fbc3f4368c56f5b2f3362ba72ecc0688797e7
Shreya Chaudhary <116176243+SleepyheadC@users.noreply.github.com>
2024-06-21 11:26:19 +0530

    complete signup page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  // we have created a proxy sever which is indicating that if you ecountered with /api anywhere in the routess
+  // go to the localhost 3000 
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
+
   plugins: [react()],
 })
