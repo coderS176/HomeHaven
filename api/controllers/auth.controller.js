964e72988d1d9959c3a6f32f4aa49aafb88fb254
Shreya Chaudhary <116176243+SleepyheadC@users.noreply.github.com>
2024-06-22 15:02:01 +0530

    add redux toolkit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import jwt  from 'jsonwebtoken';
 
+
 export const signup  = async (req, res, next) =>{
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -28,12 +29,14 @@ export const signin = async (req, res, next) => {
         const validUser = await User.findOne({email});
         if(!validUser) return next(errorHandler(404, 'User not found'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if(!validPassword) return next(errorHandler(401, 'Invalid Password!'));
+        if(!validPassword) return next(errorHandler(401, 'Wong Credentials!'));
         const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+
+        // separating rest of the information and password from the user
         const { password: pass, ...rest } = validUser._doc;
         res.cookie('access_token', token, {httpOnly:true}).status(200).json(rest);
     }
     catch(error){
         next(error);
     }
-}
+};
